192e0d79a41d2284307733165c6ae30a8f0a4127
Polina <polinka256@gmail.com>
2022-02-10 20:13:00 +0300

    feat: answer

--- src/index.js
+++ src/index.js
@@ -1,124 +1,123 @@
-//module.exports = 
-function toReadable (number) {
-    let first = {
-        0: 'zero',
-        1: 'one',
-        2: 'two',
-        3: 'three',
-        4: 'four',
-        5: 'five',
-        6: 'six',
-        7: 'seven',
-        8: 'eight',
-        9: 'nine'
-    };
-    let second = {
-        11: 'eleven',
-        12: 'twelve',
-        13: 'thirteen',
-        14: 'fourteen',
-        15: 'fifteen',
-        16: 'sixteen',
-        17: 'seventeen',
-        18: 'eighteen',
-        19: 'nineteen'
-    };
-    let third = {
-        10: 'ten',
-        20: 'twenty',
-        30: 'thirty',
-        40: 'forty',
-        50: 'fifty',
-        60: 'sixty',
-        70: 'seventy',
-        80: 'eighty',
-        90: 'ninety'
-    };
-    let forth = {
-        100: 'hundred',
-        1000: 'thousand'
-    };
-    number = String(number);
-    let counter = '';
-
-    let len1 = function() { 
-        for (let key in first) {
-            if (key == Number(number)) return first[key];
-            counter += first[key];
-        }
-    }
-    let len2 = function() {
-        for (let key1 in third) {
-            if (key1 == Number(number)) return third[key1];
-            counter += third[key1];
-        }
-    }
-    let len3 = function() {
-        for (let key2 in second) {
-            if (key2 == Number(number)) return second[key2];
-            counter += second[key2];
-        }
-    }
-    let len4 = function() {
-        let e = number.slice(0,1);
-        for (let key in third) {
-            let c = e + '0';
-            if (key == Number(c)) return third[key];
-            counter += third[key];
-        } 
-    }
-    let len8 = function() {
-        let h = number.slice(1);
-        for (let key in first) {
-            if (key == Number(h)) return first[key];
-            counter += first[key];
-        } 
-    }
-    let len5 = function() {
-        for (let key1 in forth) {
-            if (key1 == Number(number)) return forth[key1];
-            counter += forth[key1];
-        }
-    }
-    let len6 = function() {
-        let F = number.slice(0,1);
-            for (let key in first) {
-                if (Number(F) == key) return first[key];
-                counter += first[key];
-            }
-    }
-
-
-    let len9 = function() {
-       
-            let n = '';
-            let z = number.slice(0,1);
-            n += len4(z);
-            let q = '';
-            let m = number.slice(1);
-            q += len6(m);
-            counter += n + q;
-
-    }
-
-
-
-    if (number.length == 1) return len1(number);
-    if (number.length == 2) {
-        if (Number(number) % 10 == 0) return len2(number)
-        if (Number(number) > 10 && Number(number) < 20) return len3(number)
-        if (Number(number) > 20) return len4(number) + " " + len8(number)
-    }
-    if (number.length == 3) {
-        if (Number(number) == 100) return len5(number)
-        if (Number(number) % 100 == 0 && Number(number) !== 100) return len6(number) + " hundred"
-        if (Number(number) % 10 == 0) return len9(number)
-    }
-    if (number.length == 4) {
-        if (Number(number) == 1000) return len5(number)
-    }
+module.exports = function toReadable (number) {
+    let object1 = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
+      }
       
+      if(number === 0) {
+          return 'zero';
+      }
+
+      number = String(number);
+    
+      if(number.length == 1) {
+          for(let key in object1) {
+              if(number == key) {
+                  number = object1[key];
+                  return number;
+              }
+          }
+      }
+    
+      if(number.length == 2 && +number >= 20) {
+        let firstDigit = number.slice(0,1) + "0";
+        let secondDigit = number.slice(1);
+        let arr = [];
+        for(let key in object1) {
+            if(firstDigit == key) {
+                firstDigit = object1[key];  
+                arr.push(firstDigit);
+            }
+        }
+    
+        for(let key in object1) {
+            if(secondDigit == key) {
+                secondDigit = object1[key];  
+                arr.push(secondDigit);
+            }
+        }
+        arr = arr.join(" ");
+        return arr;
+      } else if(number.length == 2 && +number < 20) {
+        for(let key in object1) {
+            if(number == key) {
+                number = object1[key];  
+                return number;
+            }
+        }
+      }
+    
+      if(number.length == 3) {
+          let firstD = number.slice(0,1);
+          let arr2 = [];
+          for(let key in object1) {
+              if(firstD == key) {
+                  firstD = object1[key];
+                  arr2.push(firstD);
+                  arr2.push('hundred');
+              }
+          }
+          let nextD = number.slice(1);
+          
+  
+          if(+nextD >= 10 && +nextD < 20) {
+              for(let key in object1) {
+                  if(nextD == key) {
+                      nextD = object1[key];
+                      arr2.push(nextD);
+                  }
+              }
+          } else if(+nextD >= 20) {
+              let nextD1 = nextD.slice(0,1) + "0";
+              let nextD2 = nextD.slice(1);
+              for(let key in object1) {
+                  if(nextD1 == key) {
+                      nextD1 = object1[key];  
+                      arr2.push(nextD1);
+                  }
+              }
+          
+              for(let key in object1) {
+                  if(nextD2 == key) {
+                      nextD2 = object1[key];  
+                      arr2.push(nextD2);
+                  }
+              }
+          } else if(+nextD < 10) {
+              for(let key in object1) {
+                  if(nextD.slice(1) == key) {
+                      nextD = object1[key];
+                      arr2.push(nextD);
+                  }
+              }
+          }
+  
+          return arr2.join(' ');
+  
+      }
 }
-
-
-console.log(toReadable (330))
